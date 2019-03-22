8aa00bc20083c7179a627908f9e1e6c1a6d3a23a
61606823 <61606823@qq.com>
2019-03-22 13:30:04 +0800

    add

--- src/main/java/com/aska/mybatisdemo/config/DataSourceConfig.java
+++ src/main/java/com/aska/mybatisdemo/config/DataSourceConfig.java
@@ -5,13 +5,11 @@ import com.aska.mybatisdemo.common.dataSource.DataBaseType;
 import com.aska.mybatisdemo.common.dataSource.DatabaseContextHolder;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.core.env.Environment;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
@@ -22,9 +20,6 @@ import java.util.Map;
 
 @Configuration
 public class DataSourceConfig {
-    @Autowired
-    private Environment env;
-
     @Primary
     @Bean
     @ConfigurationProperties(prefix = "spring.datasource.master")
