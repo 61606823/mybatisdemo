ffe3f73a2cb51da075acae1f5015886a4fe1676e
61606823 <61606823@qq.com>
2019-05-20 15:33:02 +0800

    DataSourceAspect update

--- src/main/java/com/aska/mybatisdemo/aop/DataSourceAspect.java
+++ src/main/java/com/aska/mybatisdemo/aop/DataSourceAspect.java
@@ -9,19 +9,19 @@ import org.springframework.stereotype.Component;
 @Component
 public class DataSourceAspect {
     @Before("(@annotation(com.aska.mybatisdemo.annotation.Master) " +
-            "|| execution(* com.aska.mybatisdemo.service..*.insert*(..)) " +
-            "|| execution(* com.aska.mybatisdemo.service..*.update*(..)) " +
-            "|| execution(* com.aska.mybatisdemo.service..*.delete*(..)) " +
-            "|| execution(* com.aska.mybatisdemo.service..*.add*(..))) " +
+            "|| execution(* com.aska.mybatisdemo.mapper..*.insert*(..)) " +
+            "|| execution(* com.aska.mybatisdemo.mapper..*.update*(..)) " +
+            "|| execution(* com.aska.mybatisdemo.mapper..*.delete*(..)) " +
+            "|| execution(* com.aska.mybatisdemo.mapper..*.add*(..))) " +
             "&& !@annotation(com.aska.mybatisdemo.annotation.Slave)")
     public void setMasterDataSourceType() {
         DatabaseContextHolder.setMaster();
     }
 
     @Before("(@annotation(com.aska.mybatisdemo.annotation.Slave) " +
-            "|| execution(* com.aska.mybatisdemo.service..*.query*(..)) " +
-            "|| execution(* com.aska.mybatisdemo.service..*.select*(..)) " +
-            "|| execution(* com.aska.mybatisdemo.service..*.get*(..))) " +
+            "|| execution(* com.aska.mybatisdemo.mapper..*.query*(..)) " +
+            "|| execution(* com.aska.mybatisdemo.mapper..*.select*(..)) " +
+            "|| execution(* com.aska.mybatisdemo.mapper..*.get*(..))) " +
             "&& !@annotation(com.aska.mybatisdemo.annotation.Master)")
     public void setSlaveDataSourceType() {
         DatabaseContextHolder.setSlave();
